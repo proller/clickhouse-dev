c308d108da09cf4a1e03a301cb9979ac444202a6
proller <proller@github.com>
2017-07-21 22:50:44 +0300

    wip

--- test_external.sh
+++ test_external.sh
@@ -4,4 +4,3 @@ pwd=`pwd`
 #./clickhouse-test --no-shard -c "../../build/dbms/src/Server/clickhouse --client --config ${pwd}/config-client.xml "
 cd ../dbms/tests/external_dictionaries/
 ./generate_and_test.py --port=19000 --no_break --no_mongo  --no_mysql
-#
