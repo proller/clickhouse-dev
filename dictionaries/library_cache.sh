09643d70d4bb4834e1f40d38fc2102a9bbe19f8c
proller <proller@github.com>
2017-08-31 19:33:45 +0300

    wip

--- dictionaries/library_cache.sh
+++ dictionaries/library_cache.sh
@@ -3,7 +3,7 @@
 pwd=`pwd`
 cd ..
 ./cmake.sh
-make -C ../build dictionary_library dictionary_library_c dictionary_library_empty && \
+make -C ../build dictionary_library dictionary_library_c dictionary_library_empty
 sh -x ./with_server.sh "cat $pwd/library_cache.sql $pwd/later.sql | grep -v '^--' | clickhouse --client --config=clickhouse-client.xml -m -n"
 
 #sh -x ./with_server.sh "cat $pwd/hier.sql | grep -v '^--' | clickhouse --client --config=clickhouse-client.xml -m -n"
