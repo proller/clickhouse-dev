94cc13f857c43943b2794f76f280300e25188e5a
proller <proller@github.com>
2017-09-13 16:34:05 +0300

    wip

--- server.sh
+++ server.sh
@@ -2,5 +2,5 @@
 
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # ${pwd}/build.sh
-${pwd}/build$BUILD_TYPE/dbms/src/Server/clickhouse --config=config.xml $*
+${pwd}/../build$BUILD_TYPE/dbms/src/Server/clickhouse --config=config.xml $*
 
