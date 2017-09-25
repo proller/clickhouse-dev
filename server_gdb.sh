beb5768f5d61060ae154a2290b814b3038ef72aa
proller <proller@github.com>
2017-09-25 22:16:59 +0300

    wip

--- server_gdb.sh
+++ server_gdb.sh
@@ -2,6 +2,6 @@
 set -e
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-export BUILD_TYPE=_debug
+#export BUILD_TYPE=_debug
 $CURDIR/build.sh
-gdb -ex run --args ../build${BUILD_TYPE}/dbms/src/Server/clickhouse --config=config.xml
+gdb -ex run --args $CURDIR/../build${BUILD_TYPE}/dbms/src/Server/clickhouse server --config=config.xml
