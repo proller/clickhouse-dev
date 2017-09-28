10c6805fbbf78c5ec3d2314d5c225946e8d9b44d
proller <proller@github.com>
2017-09-28 16:54:53 +0300

    wip

--- client.sh
+++ client.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-../build${BUILD_TYPE}/dbms/src/Server/clickhouse --client --config `pwd`/clickhouse-client.xml $*
+pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+${pwd}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse --client --config ${pwd}/clickhouse-client.xml $*
--- make.sh
+++ make.sh
@@ -3,4 +3,5 @@ set -e
 
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 mkdir -p ${pwd}/../build$BUILD_TYPE && cd ${pwd}/../build$BUILD_TYPE
+# env -u CCACHE_PREFIX
 TIME="\t%e,\t%M" /usr/bin/time -v nice -n20 make -j -l$(nproc || sysctl -n hw.ncpu || echo 2) clickhouse $*
