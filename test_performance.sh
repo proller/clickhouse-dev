6e132cecbbdabc1e61e3791e9447c6197bd567ca
proller <proller@github.com>
2017-10-06 20:22:07 +0300

    wip

--- test_performance.sh
+++ test_performance.sh
@@ -3,4 +3,4 @@
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # --config `pwd`/clickhouse-client.xml
-${pwd}/../build$BUILD_TYPE/dbms/src/Server/clickhouse --performance-test --port `${pwd}/../build$BUILD_TYPE/dbms/src/Server/clickhouse extract-from-config --config-file=${pwd}/config.xml --key=tcp_port` --r ${pwd}/../dbms/tests/performance $* > performance.js
+time ${pwd}/../build$BUILD_TYPE/dbms/src/Server/clickhouse --performance-test --port `${pwd}/../build$BUILD_TYPE/dbms/src/Server/clickhouse extract-from-config --config-file=${pwd}/config.xml --key=tcp_port` --r ${pwd}/../dbms/tests/performance $* > performance.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
