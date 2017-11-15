36e7d78f8ca096fc26701d1843c3ebaf10d5a036
proller <proller@github.com>
2017-11-15 16:20:22 +0300

    wip

--- test_performance.sh
+++ test_performance.sh
@@ -11,4 +11,4 @@ fi
 extract="${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse extract-from-config --config-file=${CURDIR}/config.xml"
 
 TIME="\t%e,\t%M" /usr/bin/time $TIMEV httperf --num-conns=1000 --rate=1000 --port=`$extract --key=http_port` --uri=/?query=SELECT%201
-TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse --performance-test --port `$extract --key=tcp_port` --r ${CURDIR}/../dbms/tests/performance $* > performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
+TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse --performance-test --port `$extract --key=tcp_port` --r ${CURDIR}/../dbms/tests/performance $* > ${CURDIR}/performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
