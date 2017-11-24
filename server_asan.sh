54c8afaa1f5647ce9b7c7a993d1d39631eb0c479
proller <proller@github.com>
2017-11-24 22:20:01 +0300

    wip

--- server_asan.sh
+++ server_asan.sh
@@ -5,4 +5,4 @@ export BUILD_TYPE=${BUILD_TYPE:="_asan"}
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $pwd/cmake${BUILD_TYPE}.sh
 $pwd/make.sh
-$pwd/server.sh
+env ASAN_OPTIONS=detect_odr_violation=0 $pwd/server.sh
--- server_clang_asan.sh
+++ server_clang_asan.sh
@@ -3,8 +3,8 @@ set -e
 
 export BUILD_TYPE=${BUILD_TYPE:="_clang_asan"}
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-$pwd/cmake_clang.sh -DCMAKE_BUILD_TYPE=Asan -DNO_WERROR=1 -DENABLE_LIBTCMALLOC=0 -DENABLE_UNWIND=0 -DUNBUNDLED=0
+$pwd/cmake_clang.sh -DCMAKE_BUILD_TYPE=Asan -DNO_WERROR=0 -DENABLE_LIBTCMALLOC=0 -DENABLE_UNWIND=0 -DUNBUNDLED=0
 $pwd/make.sh
-$pwd/server.sh
+env ASAN_OPTIONS=detect_odr_violation=0 $pwd/server.sh
 
 #  | llvm-symbolizer-devel | c++filt
