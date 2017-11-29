3e89d83adeb3e6711915f63004b39b262875aa0f
proller <proller@github.com>
2017-11-29 15:38:24 +0300

    wip

--- server_clang_tsan.sh
+++ server_clang_tsan.sh
@@ -4,7 +4,7 @@ set -e
 export BUILD_TYPE=${BUILD_TYPE:="_clang_tsan"}
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-$pwd/cmake_clang.sh -DCMAKE_BUILD_TYPE=Tsan -DNO_WERROR=1 -DENABLE_LIBTCMALLOC=0 -DENABLE_UNWIND=0 -DUNBUNDLED=0
+$pwd/cmake_clang.sh -DCMAKE_BUILD_TYPE=Tsan -DNO_WERROR=1 -DENABLE_TCMALLOC=0 -DENABLE_UNWIND=0 -DUNBUNDLED=0
 
 $pwd/make.sh
 $pwd/server.sh
