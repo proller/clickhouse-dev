2871b5d3905e8bad439f86ccd32c07c230412fba
proller <proller@github.com>
2017-12-11 15:03:09 +0300

    wip

--- server_gdb.sh
+++ server_gdb.sh
@@ -11,6 +11,11 @@ export ASAN_OPTIONS=${ASAN_OPTIONS:="abort_on_error=1"}
 GDB=${GDB:=`bash -c "compgen -c gdb | grep 'gdb[[:digit:]]' | sort --version-sort --reverse | head -n1"`}
 GDB=${GDB:=gdb}
 
+LLDB=${LLDB:=`bash -c "compgen -c lldb | grep 'lldb[[:digit:]]' | sort --version-sort --reverse | head -n1"`}
+LLDB=${LLDB:=lldb}
+
+#$LLDB -ex run $CURDIR/../build${BUILD_TYPE}/dbms/src/Server/clickhouse -- server --config=config.xml
+
 $GDB -ex run --args $CURDIR/../build${BUILD_TYPE}/dbms/src/Server/clickhouse server --config=config.xml
 
 # -ex 'set pagination off' -ex 'set logging on' -ex "set logging file gdb.log" -ex 'continue' -ex 'thread apply all backtrace' -ex 'detach' -ex 'quit'
