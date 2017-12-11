9b19ebee300c594c25f2b074e4ff57913d6005a4
proller <proller@github.com>
2017-12-11 22:26:32 +0300

    wip

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 cd ${CURDIR}/../dbms/tests/
 
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
