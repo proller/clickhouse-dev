1a92b6760aaad1489e449dce31a1594ec89789ce
proller <proller@github.com>
2017-12-11 20:12:36 +0300

    wip

--- make.sh
+++ make.sh
@@ -11,4 +11,5 @@ pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 mkdir -p ${pwd}/../build$BUILD_TYPE && cd ${pwd}/../build$BUILD_TYPE
 # env -u CCACHE_PREFIX
 
-TIME="\t%e,\t%M" /usr/bin/time $TIMEV nice -n20 make -j$(nproc || sysctl -n hw.ncpu || echo 2) clickhouse $*
+# env -u CCACHE_PREFIX
+TIME="\t%e,\t%M" /usr/bin/time $TIMEV nice -n20 make -j$(nproc || sysctl -n hw.ncpu || echo 2) clickhouse-server clickhouse-client clickhouse $*
