4c06e57c52cce70e0ccb6b2c3db28a73ae52dab8
proller <proller@github.com>
2017-12-11 19:38:35 +0300

    wip

--- server_release.sh
+++ server_release.sh
@@ -4,6 +4,6 @@ set -e
 
 export BUILD_TYPE=${BUILD_TYPE:="_release"}
 pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-$pwd/cmake.sh -DCMAKE_BUILD_TYPE=RELWITHDEBINFO
+$pwd/cmake.sh -DUSE_STATIC_LIBRARIES=1 -DSPLIT_SHARED_LIBRARIES=0 -DCLICKHOUSE_SPLIT_BINARY=0 -DCMAKE_BUILD_TYPE=RELWITHDEBINFO
 $pwd/make.sh VERBOSE=1
 $pwd/server.sh
