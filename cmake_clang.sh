9371535fc487ef4c9eb8db26db26f3ce66f45d53
proller <proller@github.com>
2017-12-11 21:44:09 +0300

    wip

--- cmake_clang.sh
+++ cmake_clang.sh
@@ -17,7 +17,7 @@ CXX=${CXX:=`bash -c "compgen -c clang++ | grep 'clang++$MINUS[[:digit:]]' | sort
 
 mkdir -p $CURDIR/../build$BUILD_TYPE && cd $CURDIR/../build$BUILD_TYPE
 
-$CURDIR/cmake.sh -DCMAKE_CXX_COMPILER=`which ${CXX}` -DCMAKE_C_COMPILER=`which ${CC}` -DUSE_STATIC_LIBRARIES=0 -DCMAKE_BUILD_TYPE=Debug $*
+$CURDIR/cmake.sh -DCMAKE_CXX_COMPILER=`which ${CXX}` -DCMAKE_C_COMPILER=`which ${CC}` -DCMAKE_ASM_COMPILER=`which ${CC}` -DUSE_STATIC_LIBRARIES=0 -DCMAKE_BUILD_TYPE=Debug $*
 
 
 #cmake .. -DCMAKE_CXX_COMPILER=`which clang++-4.0` -DCMAKE_C_COMPILER=`which clang-4.0` -DUSE_STATIC_LIBRARIES=0 -DCMAKE_BUILD_TYPE=Debug
