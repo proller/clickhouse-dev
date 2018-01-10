c6fd52def9d762400ae30b3a44751154f38d44fa
proller <proller@github.com>
2018-01-10 19:55:58 +0300

    wip

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
@@ -12,12 +11,14 @@ else
     MAKEL="-j $(nproc || sysctl -n hw.ncpu || echo 2)"
 fi
 
-if [[ `distcc --show-hosts | wc -l` == "0" ]]; then
+if [[ $(`distcc --show-hosts | wc -l` || echo 0) == "0" ]]; then
     export -n CCACHE_PREFIX
     MAKEJ="-j $(nproc || sysctl -n hw.ncpu || echo 2)"
 else
-    MAKEJ="-j `distcc -j`"
+    MAKEJ="-j $(distcc -j || echo 0)"
 fi
+
+set -e
 
 cd $BUILD_DIR
 # env -u CCACHE_PREFIX
--- test_performance.sh
+++ test_performance.sh
@@ -14,4 +14,4 @@ extract="${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse-extract-from-c
 #$extract --key=zookeeper
 #exit
 TIME="\t%e,\t%M" /usr/bin/time $TIMEV httperf --num-conns=1000 --rate=1000 --port=`$extract --key=http_port` --uri=/?query=SELECT%201
-TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse-performance-test --port `$extract --key=tcp_port` --r ${CURDIR}/../dbms/tests/performance $* > ${CURDIR}/performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
+TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CURDIR}/../build$BUILD_TYPE/dbms/src/Server/clickhouse-performance-test --port `$extract --key=tcp_port` --r ${CURDIR}/../dbms/tests/performance --skip-tags=long $* > ${CURDIR}/performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
