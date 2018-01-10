8f175b9828e83b4bcd456d8376b9bd5c098e371d
proller <proller@github.com>
2018-01-10 21:26:47 +0300

    wip

--- server_clang_tsan.sh
+++ server_clang_tsan.sh
@@ -6,6 +6,10 @@ pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 . $pwd/cmake_clang.sh -DCMAKE_BUILD_TYPE=Tsan -DENABLE_TCMALLOC=0 -DENABLE_UNWIND=0 -DUNBUNDLED=0 \
  $*
-# -DNO_WERROR=1 -DENABLE_RDKAFKA=0
+
+# -DNO_WERROR=1 -DENABLE_RDKAFKA=0 -DCCACHE_FOUND=0
+
+
 . $pwd/make.sh
+export TSAN_OPTIONS=second_deadlock_stack=1
 . $pwd/server.sh
