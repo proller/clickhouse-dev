b017c4235b3cf8df912bab7fed67774ec5d8e0c2
proller <proller@github.com>
2018-02-13 21:48:37 +0300

    wip

--- test_performance.sh
+++ test_performance.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-export BUILD_TYPE=${BUILD_TYPE:="_debug"}
+export BUILD_TYPE=${BUILD_TYPE="_debug"}
 
 if [[ "$OSTYPE" == "FreeBSD"* ]]; then
     TIMEV=""
---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+UPDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd ${CURDIR}/..
+
+git remote add upstream https://github.com/yandex/ClickHouse.git
+git pull && git merge upstream/master && git push
+
+cd ${CURDIR}
