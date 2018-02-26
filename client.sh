a6d841de1ae87e97126fd884ae06632999a391bc
proller <proller@github.com>
2018-02-26 18:15:35 +0300

    wip

--- client.sh
+++ client.sh
@@ -2,5 +2,5 @@
 
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 
-pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-${pwd}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config ${pwd}/clickhouse-client.xml $*
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+${CUR_DIR}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config ${CUR_DIR}/clickhouse-client.xml $*
--- dictionaries/library_cache.sh
+++ dictionaries/library_cache.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
-pwd=`pwd`
-cd ..
-./cmake.sh
-make -C ../build dictionary_library dictionary_library_c dictionary_library_empty
-sh -x ./with_server.sh "cat $pwd/library_cache.sql $pwd/later.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
+DCURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-#sh -x ./with_server.sh "cat $pwd/hier.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
-#sh -x ./with_server.sh "cat $pwd/later.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
+#cd ..
+. $DCURDIR/../cmake.sh
+. $DCURDIR/../make.sh dictionary_library dictionary_library_c dictionary_library_empty
+. $DCURDIR/../with_server.sh "cat $DCURDIR/library_cache.sql $CURDIR/later.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
+
+#sh -x ./with_server.sh "cat $DCURDIR/hier.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
+#sh -x ./with_server.sh "cat $DCURDIR/later.sql | grep -v '^--' | clickhouse-client --config=clickhouse-client.xml -m -n"
 
