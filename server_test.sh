20326ab6c4e4aee753bb783f9b9cd610900f891a
proller <proller@github.com>
2018-03-15 19:12:59 +0300

    wip

--- server_test.sh
+++ server_test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# env TEST_OPT="505" bash -x ./server_test.sh
+# env TEST_OPT="-- 99999|505" bash -x ./server_test.sh
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 
+# magic args for ctest
 export ARGS="-V -j $(nproc || sysctl -n hw.ncpu || echo 2)"
+
 . ${CURDIR}/build.sh
 . $CURDIR/make.sh test
