d38e4fc193f27e80899024f7ad6a7368aa729f67
proller <proller@github.com>
2018-04-03 20:44:01 +0300

    wip

--- client.sh
+++ client.sh
@@ -4,3 +4,5 @@ export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ${CUR_DIR}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config ${CUR_DIR}/clickhouse-client.xml --stacktrace $*
+
+# cat ../dbms/tests/queries/0_stateless/00700_extract.sql | ./client.sh -n --ignore-error
