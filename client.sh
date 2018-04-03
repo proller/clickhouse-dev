d53ad8fb69eac8f10e523c2d507cb5d62d6cc2ef
proller <proller@github.com>
2018-04-03 20:42:48 +0300

    wip

--- client.sh
+++ client.sh
@@ -3,4 +3,4 @@
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-${CUR_DIR}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config ${CUR_DIR}/clickhouse-client.xml $*
+${CUR_DIR}/../build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config ${CUR_DIR}/clickhouse-client.xml --stacktrace $*
