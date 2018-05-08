327f26e4e8f4c1b1a78fd662365aac8d6f2f8da9
proller <proller@github.com>
2018-05-08 22:01:29 +0300

    wip

--- test_all.sh
+++ test_all.sh
@@ -4,22 +4,29 @@ set -x
 #set -e
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-export BUILD_TYPE=${BUILD_TYPE="_debug"}
+
+for type in _debug _clang clang_asan _asan _release; do
+
+export BUILD_TYPE=$type
 #sh $CUR_DIR/build.sh all
 . $CUR_DIR/make.sh all
 . $CUR_DIR/ctest.sh
 
-export BUILD_TYPE=${BUILD_TYPE="_clang"}
+done
+
+
+exit
+export BUILD_TYPE=_clang
 #sh $CUR_DIR/build.sh all
 . $CUR_DIR/make.sh all
 . $CUR_DIR/ctest.sh
 
-export BUILD_TYPE=${BUILD_TYPE="clang_asan"}
+export BUILD_TYPE=clang_asan
 #sh $CUR_DIR/build.sh all
 . $CUR_DIR/make.sh all
 . $CUR_DIR/ctest.sh
 
-export BUILD_TYPE=${BUILD_TYPE="_asan"}
+export BUILD_TYPE=_asan
 #sh $CUR_DIR/build.sh all
 . $CUR_DIR/make.sh all
 . $CUR_DIR/ctest.sh
