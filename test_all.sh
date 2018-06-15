ec785489b70d852198420c13a0fee99ab67eed29
proller <proller@github.com>
2018-06-15 19:19:12 +0300

    wip

--- test_all.sh
+++ test_all.sh
@@ -7,6 +7,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 for type in _debug _clang _clang_asan _clang_tsan _asan _tsan _release; do
     export BUILD_TYPE=$type
+    #bash $CUR_DIR/build.sh
     bash $CUR_DIR/build.sh && bash $CUR_DIR/ctest.sh || true
     #bash $CUR_DIR/make.sh all
 done
