c07a87e0e3c506bd0639dd12fd457a8c74194c06
proller <proller@github.com>
2018-07-12 19:06:01 +0300

    wip

--- server_release.sh
+++ server_release.sh
@@ -6,6 +6,6 @@ export BUILD_TYPE=${BUILD_TYPE="_release"}
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/cmake_release.sh $*
 # -DUSE_STATIC_LIBRARIES=1 -DSPLIT_SHARED_LIBRARIES=0 -DCLICKHOUSE_SPLIT_BINARY=0 -DCMAKE_BUILD_TYPE=RELWITHDEBINFO -DENABLE_RDKAFKA=0
-. $CURDIR/make.sh
+. $CURDIR/make.sh clickhouse-bundle
 # VERBOSE=1
 . $CURDIR/server.sh
