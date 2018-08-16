a4899e4c0542a2a5ad7c92a2321b4d3368f8b95e
proller <proller@github.com>
2018-08-16 22:02:50 +0300

    wip

--- test_all.sh
+++ test_all.sh
@@ -9,7 +9,7 @@ for type in _debug _clang _clang_asan _clang_tsan _asan _tsan _release; do
     echo "Run $type"
     export BUILD_TYPE=$type
     #bash $CUR_DIR/build.sh
-    bash $CUR_DIR/build.sh && bash $CUR_DIR/ctest.sh || true
+    bash $CUR_DIR/build.sh | tee log-build$type.log && bash $CUR_DIR/ctest.sh | tee log-ctest$type.log || true
     #bash $CUR_DIR/make.sh all
     echo "Done $type"
 done
