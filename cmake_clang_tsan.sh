7c2aec2a8746085bc5ecb80a5402b9d3373ce201
proller <proller@github.com>
2018-08-27 23:42:07 +0300

    wip

--- cmake_clang_tsan.sh
+++ cmake_clang_tsan.sh
@@ -2,4 +2,4 @@
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_clang_asan"}
-. $CURDIR/cmake_clang.sh -DSANITIZE=address -DUNBUNDLED=0 $*
+. $CURDIR/cmake_clang.sh -DSANITIZE=thread -DUNBUNDLED=0 $*
