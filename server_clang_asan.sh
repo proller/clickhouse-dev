0008aa779fb099be55c92c1c8abdd135db4c7ed7
proller <proller@github.com>
2018-09-01 17:41:44 +0300

    wip

--- server_clang_asan.sh
+++ server_clang_asan.sh
@@ -2,16 +2,16 @@
 set -e
 
 export BUILD_TYPE=${BUILD_TYPE:="_clang_asan"}
-pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-. $pwd/cmake_clang.sh -DSANITIZE=address -DUNBUNDLED=0 \
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. $CUR_DIR/cmake_clang.sh -DSANITIZE=address -DUNBUNDLED=0 \
  $*
 # -DNO_WERROR=0 -DENABLE_RDKAFKA=0
-. $pwd/make.sh
+. $CUR_DIR/make.sh
 export  ASAN_OPTIONS=detect_odr_violation=0
-#. $pwd/server.sh | llvm-symbolizer-6.0 | c++filt
-. $pwd/server.sh -- --logger.console=0 2>&1 | asan_symbolize-6.0 | c++filt | tee log$BUILD_TYPE.asan.log
-cat log$BUILD_TYPE.server.stdout.log | asan_symbolize-6.0 | c++filt > log$BUILD_TYPE.server.stdout.filt.log
-#. $pwd/server_gdb.sh | llvm-symbolizer-devel | c++filt
+#. $CUR_DIR/server.sh | llvm-symbolizer-6.0 | c++filt
+. $CUR_DIR/server.sh -- --logger.console=0 2>&1 | asan_symbolize-6.0 | c++filt | tee log$BUILD_TYPE.asan.log
+cat $CUR_DIR/log$BUILD_TYPE.server.stdout.log | asan_symbolize-6.0 | c++filt > $CUR_DIR/log$BUILD_TYPE.server.stdout.filt.log
+#. $CUR_DIR/server_gdb.sh | llvm-symbolizer-devel | c++filt
 
 #  | llvm-symbolizer-devel | c++filt
 # -DENABLE_TCMALLOC=0 -DENABLE_UNWIND=0
