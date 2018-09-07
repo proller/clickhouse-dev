3f9327590634ef6b85e3e9f48e5c48026565d40d
proller <proller@github.com>
2018-09-07 20:51:02 +0300

    wip

--- build_server.sh
+++ build_server.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 set -e
 
-CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
-export MAKE_TARGET=clickhouse-server
-. ${CURDIR}/build.sh
-. ${CURDIR}/server.sh $*
+export MAKE_TARGET="clickhouse-server clickhouse-client clickhouse-extract-from-config"
+. ${CUR_DIR}/build.sh
+. ${CUR_DIR}/server.sh $*
