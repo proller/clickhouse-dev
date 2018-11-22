1fb3c5b1531de5b865a852622b075e7ed16c7b41
proller <proller@github.com>
2018-11-22 18:45:37 +0300

    wip

--- test_external.sh
+++ test_external.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
-#./clickhouse-test -c "../../clang_msan/dbms/programs/clickhouse-client --config ${CURDIR}/config-client.xml "
-#./clickhouse-test --no-shard -c "../../build/dbms/programs/clickhouse-client --config ${CURDIR}/config-client.xml "
+#./clickhouse-test -c "../../clang_msan/dbms/programs/clickhouse-client --config ${CUR_DIR}/config-client.xml "
+#./clickhouse-test --no-shard -c "../../build/dbms/programs/clickhouse-client --config ${CUR_DIR}/config-client.xml "
 
-cd ${CURDIR}/../dbms/tests/external_dictionaries/
-./generate_and_test.py --client="${CURDIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-client" --port=`${CURDIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-extract-from-config --config-file=${CURDIR}/config.xml --key=tcp_port` --no_break --no_mongo --no_mysql $*
+cd ${CUR_DIR}/../dbms/tests/external_dictionaries/
+./generate_and_test.py --client="${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-client" --port=`${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-extract-from-config --config-file=${CUR_DIR}/config.xml --key=tcp_port` --no_break --no_mongo --no_mysql $*
