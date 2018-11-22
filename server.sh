ad86f400bdac511ba1a53fe794da145feafbcbdc
proller <proller@github.com>
2018-11-22 18:37:17 +0300

    wip

--- server.sh
+++ server.sh
@@ -11,5 +11,8 @@ export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 VERSION=`${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-client --version-clean`
 SERVER_CONFIG_OPT="--compiler_headers=$BUILD_DIR/dbms/programs/clang/headers/$VERSION/ --compiler_headers=$BUILD_DIR/dbms/programs/clang/headers/$VERSION/ --compiler_headers_root=$BUILD_DIR/dbms/programs/clang/headers/$VERSION/"
 
+[ ! -f server.crt ] && openssl req -subj "/CN=localhost" -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt
+[ ! -f dhparam.pem ] && openssl dhparam -out dhparam.pem 1024
+
 #env PATH=$PATH:${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/ \
 ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-server --config=$CUR_DIR/config.xml $* -- $SERVER_CONFIG_OPT 2>&1 | tee $LOG_DIR$BUILD_TYPE.server.console.log
