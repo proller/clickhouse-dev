1f6a3d9e447fb5587fc05b54a0852586571bbeff
proller <proller@github.com>
2018-12-06 19:20:47 +0300

    wip

--- server_release.sh
+++ server_release.sh
@@ -5,6 +5,9 @@ set -e
 export BUILD_TYPE=${BUILD_TYPE="_release"}
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CUR_DIR/cmake_release.sh $*
-# -DUSE_STATIC_LIBRARIES=1 -DSPLIT_SHARED_LIBRARIES=0 -DCLICKHOUSE_SPLIT_BINARY=0 -DCMAKE_BUILD_TYPE=RELWITHDEBINFO -DENABLE_RDKAFKA=0
 . $CUR_DIR/make.sh clickhouse-bundle
+
+#strip ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse ||:
+# sudo setcap cap_net_admin=+ep ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse ||:
+
 bash $CUR_DIR/server.sh
