96288b31c1fa9875a3d6d8c12dcee2ad4a27ee04
proller <proller@github.com>
2019-02-12 20:33:58 +0300

    wip

--- server_valgrind.sh
+++ server_valgrind.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
+LOG_DIR=${LOG_DIR=$CUR_DIR/log/}
 
-valgrind --log-file=log$BUILD_TYPE.valgrind.log $CURDIR/../build${BUILD_TYPE}/dbms/programs/clickhouse-server --config=$CURDIR/config.xml
+valgrind --log-file=${LOG_DIR}log$BUILD_TYPE.valgrind.log $CUR_DIR/../build${BUILD_TYPE}/dbms/programs/clickhouse-server --config=$CUR_DIR/config.xml
 # --tool=memcheck, cachegrind, callgrind, helgrind, drd, massif, lackey, none, exp-sgcheck, exp-bbv, exp-dhat,
