0b4528b8cd3e8a67c70895c37125988ee73163dc
proller <proller@github.com>
2019-02-20 16:23:22 +0300

    wip

--- server_valgrind.sh
+++ server_valgrind.sh
@@ -5,5 +5,6 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export BUILD_TYPE=${BUILD_TYPE:="_debug"}
 LOG_DIR=${LOG_DIR=$CUR_DIR/log/}
 
+ulimit -n 100000
 valgrind --log-file=${LOG_DIR}log$BUILD_TYPE.valgrind.log $CUR_DIR/../build${BUILD_TYPE}/dbms/programs/clickhouse-server --config=$CUR_DIR/config.xml
 # --tool=memcheck, cachegrind, callgrind, helgrind, drd, massif, lackey, none, exp-sgcheck, exp-bbv, exp-dhat,
