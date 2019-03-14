c0a4578dd57d7abacec6ee99027de2edde4c9d29
proller <proller@github.com>
2019-03-14 18:25:30 +0300

    wip

--- server.sh
+++ server.sh
@@ -14,6 +14,7 @@ SERVER_CONFIG_OPT="--compiler_headers=$BUILD_DIR/dbms/programs/clang/headers/$VE
 [ ! -f server.crt ] && openssl req -subj "/CN=localhost" -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt
 [ ! -f dhparam.pem ] && openssl dhparam -out dhparam.pem 1024
 
+cd $CUR_DIR
 #env PATH=$PATH:${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/ \
 ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-server --config=$CUR_DIR/config.xml $* -- $SERVER_CONFIG_OPT 2>&1 | tee $LOG_DIR$BUILD_TYPE.server.console.log
 
--- test_all.sh
+++ test_all.sh
@@ -7,7 +7,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 LOG_DIR=${LOG_DIR=$CUR_DIR/log/}
 mkdir -p $LOG_DIR
 
-for type in _debug _clang _clang_asan _clang_tsan _asan _tsan _release; do
+for type in _debug _clang _clang_asan _clang_tsan _asan _tsan _release _split; do
     echo "Run $type"
     export BUILD_TYPE=$type
     #bash $CUR_DIR/build.sh | tee ${LOG_DIR}log$type.build.log
