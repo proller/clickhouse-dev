d372039b8aa2089b1422d68c3f276ccd826b5a2c
proller <proller@github.com>
2019-03-22 19:16:09 +0300

    wip

--- test.sh
+++ test.sh
@@ -9,6 +9,6 @@ cd ${CUR_DIR}/../dbms/tests/
 #export CLICKHOUSE_CONFIG=${CLICKHOUSE_CONFIG:=$CUR_DIR/config.xml}
 export CLICKHOUSE_TMP=${CUR_DIR}/tmp
 
-./clickhouse-test --testname --binary ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse --configclient ${CUR_DIR}/clickhouse-client.xml --configserver ${CUR_DIR}/config.xml $TEST_OPT $*
+env ASAN_OPTIONS=detect_odr_violation=0 ./clickhouse-test --testname --binary ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse --configclient ${CUR_DIR}/clickhouse-client.xml --configserver ${CUR_DIR}/config.xml $TEST_OPT $*
 
 # --no-shard --no-zookeeper --ssl --port 9440
