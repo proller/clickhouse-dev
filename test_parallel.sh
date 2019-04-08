6db3867bce2e1d04e32f978b13816eb31d6ce354
proller <proller@github.com>
2019-04-08 16:54:34 +0300

    wip

--- test_parallel.sh
+++ test_parallel.sh
@@ -3,7 +3,7 @@
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 PROC=${PROC:=$(( `nproc || sysctl -n hw.ncpu || echo 2` * 2))}
 for i in `seq 1 ${PROC}`; do
-    . ${CUR_DIR}/test.sh --order=random --testname $* &
+    . ${CUR_DIR}/test.sh --order=random --testname $* --tmp=${CUR_DIR}/tmp/tmp${i} &
 done
 
 . ${CUR_DIR}/test_performance.sh &
