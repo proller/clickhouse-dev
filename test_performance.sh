1bd9ee19eb032f2fb2398be014cb349ab8141cc4
proller <proller@github.com>
2019-06-25 20:00:23 +0300

    wip

--- test_performance.sh
+++ test_performance.sh
@@ -13,4 +13,4 @@ fi
 extract="${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-extract-from-config --config-file=${CUR_DIR}/config.xml"
 
 env TIME="\t%e,\t%M" /usr/bin/time $TIMEV httperf --num-conns=1000 --rate=1000 --port=`$extract --key=http_port` --uri=/?query=SELECT%201
-env ASAN_OPTIONS=detect_odr_violation=0 TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-performance-test --port `$extract --key=tcp_port` --r ${CUR_DIR}/../dbms/tests/performance --skip-tags=long $* > ${LOG_DIR}/performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
+env ASAN_OPTIONS=detect_odr_violation=0 TIME="\t%e,\t%M" /usr/bin/time $TIMEV ${CUR_DIR}/../build$BUILD_TYPE/dbms/programs/clickhouse-performance-test --port `$extract --key=tcp_port` --skip-tags=long --input-files ${CUR_DIR}/../dbms/tests/performance/*.xml $* > ${LOG_DIR}/performance${BUILD_TYPE}.`hostname`.`date '+%Y%M%''d%H%M%''S'`.json
