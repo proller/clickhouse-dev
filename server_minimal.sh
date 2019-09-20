a868cb081fb62f4ca53112d209a890f9091e996e
proller <proller@github.com>
2019-09-20 21:53:00 +0300

    wip

--- server_minimal.sh
+++ server_minimal.sh
@@ -4,6 +4,7 @@ set -e
 
 export BUILD_TYPE=${BUILD_TYPE="_minimal"}
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-. $CUR_DIR/cmake.sh -DENABLE_LIBRARIES=0 $*
+CMAKE_OS+=" -DENABLE_LIBRARIES=0 "
+. $CUR_DIR/cmake.sh $*
 . $CUR_DIR/make.sh clickhouse-bundle
 . $CUR_DIR/server.sh
--- server_minimal_unbundled.sh
+++ server_minimal_unbundled.sh
@@ -5,4 +5,5 @@ set -e
 export BUILD_TYPE=${BUILD_TYPE="_minimal_unbundled"}
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-. $CUR_DIR/server_minimal.sh -DUNBUNDLED=1
+CMAKE_OS+=" -DUNBUNDLED=1 "
+. $CUR_DIR/server_minimal.sh $*
